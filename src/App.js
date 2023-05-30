c788a459e20e63e4186796436d4c1de420905e7e
Olga Klimetyonok <olgaklimetenok@gmail.com>
2023-05-30 18:31:43 +0300

    feat: add pages, set-up route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/dialogs' Component={Dialogs} />
+            <Route path='/profile' Component={Profile} />
+            <Route path='/news' Component={News} />
+            <Route path='/music' Component={Music} />
+            <Route path='/settings' Component={Settings} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
