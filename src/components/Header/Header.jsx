062dc71e02aee0efcc42e5abc9c9af96af140f22
Olga Klimetyonok <olgaklimetenok@gmail.com>
2023-07-10 19:42:09 +0300

    feat: add redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import classes from './Header.module.css';
 
 const Header = () => {
     return (<header className={classes.header}>
-    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdceXmslfQjdiapvzlSPr6L5mzYEbkbRiJnQ&usqp=CAU'></img>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdceXmslfQjdiapvzlSPr6L5mzYEbkbRiJnQ&usqp=CAU' alt='pin'></img>
   </header>);
 }
 
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,37 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    messages: [{
+        id: 1,
+        message: 'Hi'
+    }, {
+        id: 2,
+        message: 'How is your it-kourses?'
+    }, {
+        id: 3,
+        message: 'How are you doing?'
+    }],
+    dialogs: [{
+        id: 1,
+        name: 'Agafia'
+    }, {
+        id: 2,
+        name: 'Arkadiy'
+    }, {
+        id: 3,
+        name: 'Nikifor'
+    }, {
+        id: 4,
+        name: 'Varvara'
+    }, {
+        id: 5,
+        name: 'Petr'
+    }],
+    newMessageBody: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body;
