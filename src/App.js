6758de3a4107cf74f1d65155948fd47bfbf5f88c
Olga Klimetyonok <olgaklimetenok@gmail.com>
2023-07-12 18:34:39 +0300

    feat: add DialogsContainer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 const App = (props) => {
   return (
@@ -23,8 +23,8 @@ const App = (props) => {
             <Route path='/music' Component={Music} />
             <Route path='/settings' Component={Settings} /> */}
 
-            <Route path='/dialogs' element={<Dialogs store={props.store} />} />
-            <Route path='/profile' element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch} store={props.store}
+            <Route path='/dialogs' element={<DialogsContainer store={props.store} />} />
+            <Route path='/profile' element={<Profile store={props.store}
             />} />
             <Route path='/news' element={<News />} />
             <Route path='/music' element={<Music />} />
